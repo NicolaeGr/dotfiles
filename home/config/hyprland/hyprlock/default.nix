7b33a90822d27db2ea6131585de83aa6cb33f879
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2024-03-05 19:35:37 +0200

    added status script

--- home/config/hyprland/hyprlock/default.nix
+++ home/config/hyprland/hyprlock/default.nix
@@ -2,7 +2,14 @@
   home.file = {
     "hyprlock.conf" = {
       target = ".config/hypr/hyprlock.conf";
-      source = "hyprlock.conf";
+      source = ./hyprlock.conf;
     };
-  }; 
+    
+    "status.sh" = {
+      target = "~/.config/hypr/hyprlock/status.sh";
+      source = ./status.sh;
+    };
+  };
+
+
 }
