4317598d672a74685a9936a6be0913d52676dc19
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2024-03-06 18:02:56 +0200

    mpd

--- modules/hardware/audio/default.nix
+++ modules/hardware/audio/default.nix
@@ -5,7 +5,7 @@
   hardware.pulseaudio.enable = false;
 
   environment.systemPackages = with pkgs; [ pulseaudio ];
-  
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -13,4 +13,16 @@
     pulse.enable = true;
     wireplumber.enable = true;
   };
+
+  # Move to it's own file
+  services.mpd = { enable = true; startWhenNeeded = true; };
+
+  hardware.pulseaudio.extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
+  services.mpd.extraConfig = ''
+    audio_output {
+      type "pulse"
+      name "Pulseaudio"
+      server "127.0.0.1" # add this line - MPD must connect to the local sound server
+    }
+  '';
 }
