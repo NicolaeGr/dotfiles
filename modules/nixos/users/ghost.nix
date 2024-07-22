301217f84184e4cefb58826a3c93de6fee7fb765
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2024-07-22 23:21:08 +0300

    added default structure for guest user

--- modules/nixos/users/ghost.nix
+++ modules/nixos/users/ghost.nix
@@ -1,1 +1,10 @@
-{ ... }: { }
+{ options, config, lib, pkgs ... }: {
+  options = {
+    users.guest.enable = lib.mkEnableOption {
+      default = false;
+      description = "Enable Nicolae's user configuration.";
+    };
+  };
+
+  config = lib.mkIf config.users.guest.enable { };
+}
