59ea8490e53afd98a31bb983d3dcc8f52881d90b
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2025-12-20 21:06:05 +0200

    Enable wireguard

--- hosts/lumix/wireguard.nix
+++ hosts/lumix/wireguard.nix
@@ -6,6 +6,39 @@
 {
   extra.wireguard.enable = true;
 
+  sops.secrets.lumix_wg_private_key = {
+    sopsFile = (configLib.relativeToRoot "secrets/wireguard.yaml");
+    format = "yaml";
+  };
+
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "10.100.0.1/24" ];
+    listenPort = config.extra.wireguard.listenPort;
+
+    privateKeyFile = config.sops.secrets.lumix_wg_private_key.path;
+
+    postUp = ''
+      sysctl -w net.ipv4.conf.all.rp_filter=0
+      sysctl -w net.ipv4.conf.default.rp_filter=0
+      sysctl -w net.ipv4.conf.wg0.rp_filter=0
+    '';
+
+    postDown = ''
+      sysctl -w net.ipv4.conf.wg0.rp_filter=2
+    '';
+
+    peers = [
+      {
+        publicKey = "DoB3CnXKA7eo+YR7FHzdk9SBzDde4mZBRWWubrSxfTU=";
+        allowedIPs = [ "10.100.0.2/32" ];
+      }
+      {
+        publicKey = "EGHSJN6pS4o7Jhx0+yBzOUUwNrDyiGFyJLdrnSYOIRA=";
+        allowedIPs = [ "10.100.0.3/32" ];
+      }
+    ];
+  };
+
   # sops.secrets.lumix_wg_private_key = {
   #   sopsFile = (configLib.relativeToRoot "secrets/wireguard.yaml");
   #   format = "yaml";
--- hosts/odin/wireguard.nix
+++ hosts/odin/wireguard.nix
@@ -6,10 +6,35 @@
 {
   extra.wireguard.enable = true;
 
-  # sops.secrets.odin_wg_private_key = {
-  #   sopsFile = (configLib.relativeToRoot "secrets/wireguard.yaml");
-  #   format = "yaml";
-  # };
+  sops.secrets.odin_wg_private_key = {
+    sopsFile = (configLib.relativeToRoot "secrets/wireguard.yaml");
+    format = "yaml";
+  };
+
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "10.100.0.2/24" ];
+
+    privateKeyFile = config.sops.secrets.odin_wg_private_key.path;
+
+    postUp = ''
+      sysctl -w net.ipv4.conf.all.rp_filter=0
+      sysctl -w net.ipv4.conf.default.rp_filter=0
+      sysctl -w net.ipv4.conf.wg0.rp_filter=0
+    '';
+
+    postDown = ''
+      sysctl -w net.ipv4.conf.wg0.rp_filter=2
+    '';
+
+    peers = [
+      {
+        publicKey = "uT3bM9N2xsDmlMMmIf4mS6PysWUxEcwAfTv3j7x1unc=";
+        endpoint = "electrolit.biz:${toString config.extra.wireguard.listenPort}";
+        allowedIPs = [ "10.100.0.0/24" ];
+        persistentKeepalive = 25;
+      }
+    ];
+  };
 
   # networking.wireguard.interfaces.wg0 = {
   #   ips = [ "10.100.0.2/24" ];
