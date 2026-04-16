6b2e44759f4779c18a6cbedcc4c06cf49199dbca
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2026-04-16 19:48:52 +0300

    Updated git conf

--- hosts/lumix/git.nix
+++ hosts/lumix/git.nix
@@ -13,4 +13,41 @@ in
       }
     ];
   };
+
+  systemd.services.auto-rebuild = {
+    description = "Auto rebuild system from dotfiles if conditions match";
+
+    serviceConfig = {
+      Type = "oneshot";
+      User = "deploy";
+    };
+
+    script = ''
+      set -euo pipefail
+
+      cd "$DOTFILES"
+
+      git diff --quiet || exit 0
+
+      git pull --tags
+
+      TAG=$(git tag --points-at HEAD | grep '^buildable-' || true)
+
+      [ -z "$TAG" ] && exit 0
+
+      just rebuild
+    '';
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "deploy" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/nixos-rebuild";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 }
