dbe3313b3d814d8d41fb014c4039295396c4e479
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2026-05-25 07:55:45 +0300

    Containarized JellyFin

--- hosts/lumix/services/jellyfin.nix
+++ hosts/lumix/services/jellyfin.nix
@@ -1,35 +1,63 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
 let
-  baseDir = "/storage/jellyfin";
+  containerLib = import ./_util.nix { inherit pkgs lib; };
+  baseDir = "/storage/appdata/jellyfin";
+  ip = "192.168.100.11";
 in
 {
-  hardware.graphics = {
+  containers.jellyfin = containerLib.mkServiceContainer {
     enable = true;
+    ip = ip;
 
-    extraPackages = with pkgs; [
-      intel-media-driver
-      libvdpau-va-gl
-    ];
+    mounts = {
+      "/storage" = {
+        hostPath = "/storage";
+        isReadOnly = false;
+      };
+
+      "/dev/dri" = {
+        hostPath = "/dev/dri";
+        isReadOnly = false;
+      };
+    };
+
+    module = {
+      hardware.graphics = {
+        enable = true;
+
+        extraPackages = with pkgs; [
+          intel-media-driver
+          libvdpau-va-gl
+        ];
+      };
+
+      services.jellyfin = {
+        enable = true;
+        openFirewall = true;
+
+        user = "deploy";
+        group = "users";
+
+        # TODO: Enable in the next nixos release
+        # hardwareAcceleration = {
+        #   enable = true;
+        #   type = "qsv";
+        #   dev = "/dev/dri/renderD128";
+        # };
+
+        cacheDir = "${baseDir}/cache";
+        configDir = "${baseDir}/config";
+        dataDir = "${baseDir}/data";
+        logDir = "${baseDir}/log";
+      };
+    };
   };
-
-  users.users.jellyfin.extraGroups = [
-    "video"
-    "render"
-  ];
-
-  services.jellyfin = {
-    enable = true;
-
-    package = pkgs.unstable.jellyfin;
-
-    dataDir = "${baseDir}/jellyfin";
-  };
-
-  environment.systemPackages = with pkgs; [
-    intel-gpu-tools
-    libva-utils
-    ffmpeg-full
-  ];
 
   services.nginx.virtualHosts."jf.electrolit.biz" = {
     forceSSL = true;
@@ -47,7 +75,7 @@ in
       # CSP
       add_header Content-Security-Policy "default-src https: data: blob:; img-src 'self' https://*; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.youtube.com blob:; worker-src 'self' blob:; connect-src 'self'; object-src 'none'; frame-ancestors 'self'; font-src 'self';" always;
 
-      set $jellyfin 127.0.0.1;
+      set $jellyfin "${ip}";
 
       location / {
         proxy_pass http://$jellyfin:8096;
