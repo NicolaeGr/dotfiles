566d34241009d6eeeeb1c787b05104c95618c396
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2026-06-16 09:49:24 +0300

    moved vscode to latest instead of locked version

--- home/_common/extra/gui/dev.nix
+++ home/_common/extra/gui/dev.nix
@@ -23,11 +23,11 @@
     programs.vscode.package = pkgs.unstable.vscode.overrideAttrs (oldAttrs: {
       src = (
         builtins.fetchTarball {
-          url = "https://update.code.visualstudio.com/1.123.0/linux-x64/stable";
-          sha256 = "sha256:0k8g7c1a386p9fji5k4mzb4an4mkfycx03ki9b573xi0a94c8xm3";
+          url = "https://update.code.visualstudio.com/latest/linux-x64/stable";
+          sha256 = "sha256:0pzid4gh2niqc3j5d30cgy1jzarlbc7pyq4lpjh7436mbm45j9f5";
         }
       );
-      version = "1.123.0";
+      version = "latest";
       buildInputs = oldAttrs.buildInputs ++ [
         pkgs.krb5
         pkgs.libsoup_3
