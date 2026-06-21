94de189b71e9dd2674e708184239f3566f079b58
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2026-06-21 12:41:34 +0300

    networking

--- hosts/lumix/router.nix
+++ hosts/lumix/router.nix
@@ -2,7 +2,8 @@
 {
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
-    "net.bridge.bridge-nf-call-iptables" = 0;
+    "net.bridge.bridge-nf-call-iptables" = 1;
+    "net.bridge.bridge-nf-call-ip6tables" = 1;
   };
   networking = {
     networkmanager.unmanaged = [
